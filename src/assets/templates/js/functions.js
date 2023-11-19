939f4341f8dd729d7fff02b8e8f37f1751dcede8
samarin2101 <servera-center@yandex.ru>
2023-11-19 22:02:15 +0300

    gulp-start

---
+++ src/assets/templates/js/functions.js
@@ -0,0 +1,39 @@
+/* Табы */
+
+// import * as Tabs from './vendors/tabs.js'
+// Tabs.tabs()
+
+
+/* Модальное окно */
+
+// import * as Popup from './vendors/popup.js'
+// Popup.popup()
+
+/* Burger */
+// import * as Burger from './vendors/burger.js'
+// Burger.burger()
+
+/* Валидация полей */
+
+// import * as Validations from './vendors/validations.js'
+// Validations.validations()
+
+/* Отложенная загрузка youtubes */
+
+// import * as Youtubes from './vendors/youtubes.js'
+// Youtubes.youtubes()
+
+/* Аккордеон  */
+
+// import * as Accordion from './vendors/accordion.js'
+// Accordion.accordion();
+
+
+
+
+
+
+
+
+
+
